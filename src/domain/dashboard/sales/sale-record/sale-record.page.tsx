e357b15eb475ee29a2c83c95ab934bbf75d6c44a
guiesmeril <guiesmeril@hotmail.com>
2022-07-19 20:25:02 -0300

    fix: correction tableCell

--- src/domain/dashboard/sales/sale-record/sale-record.page.tsx
+++ src/domain/dashboard/sales/sale-record/sale-record.page.tsx
@@ -347,7 +347,6 @@ export const SaleRecord = () => {
                           </TableRow>
                         </TableBody>
                       )
-
                     })}
                   </>
                 )}
