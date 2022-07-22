0ae2860f9c9d2eb5443c2b80ea2c88c25d153f33
Matheus Ferreira <teteu.andrade06@gmail.com>
2022-07-22 12:40:49 -0300

    Admin: checkout statistics

--- src/domain/admin/financial/checkout/checkout.page.tsx
+++ src/domain/admin/financial/checkout/checkout.page.tsx
@@ -1,5 +1,67 @@
-import { MainContent } from '@domain/admin/admin.styles';
+import { CardTitle, MainContent } from '@domain/admin/admin.styles';
+import { ResumeCard } from '@domain/admin/components/resume-card/resume-card.component';
+import { Box } from '@mui/material';
+import { useMedia } from '@shared/hooks/useMedia';
+import {
+  AiOutlineFileDone,
+  AiOutlineFileExcel,
+  AiOutlineSwap,
+} from 'react-icons/ai';
+import { BsCartCheck, BsCartX } from 'react-icons/bs';
+import { TbFileInvoice } from 'react-icons/tb';
 
 export const Checkout = () => {
-  return <MainContent>Checkout Page</MainContent>;
+  const mobile = useMedia('(max-width: 700px)');
+
+  return (
+    <MainContent>
+      <CardTitle>Perfomance</CardTitle>
+      <Box
+        style={{
+          width: '100%',
+          maxWidth: '1200px',
+          display: 'flex',
+          gap: '2rem',
+          flexWrap: mobile ? 'wrap' : 'initial',
+        }}
+      >
+        <ResumeCard icon={<TbFileInvoice />} label="Checkouts abertos" />
+        <ResumeCard icon={<AiOutlineFileDone />} label="Checkouts concluidos" />
+        <ResumeCard
+          icon={<AiOutlineFileExcel />}
+          label="Checkouts cancelados"
+        />
+      </Box>
+
+      <CardTitle>Cartão de crédito</CardTitle>
+      <Box
+        style={{
+          width: '100%',
+          maxWidth: '1200px',
+          display: 'flex',
+          gap: '2rem',
+          flexWrap: mobile ? 'wrap' : 'initial',
+        }}
+      >
+        <ResumeCard icon={<BsCartCheck />} label="Compras aprovadas" />
+        <ResumeCard icon={<BsCartX />} label="Compras recusadas" />
+        <ResumeCard icon={<AiOutlineSwap />} label="% Conversões" />
+      </Box>
+
+      <CardTitle>Cartão de debito</CardTitle>
+      <Box
+        style={{
+          width: '100%',
+          maxWidth: '1200px',
+          display: 'flex',
+          gap: '2rem',
+          flexWrap: mobile ? 'wrap' : 'initial',
+        }}
+      >
+        <ResumeCard icon={<BsCartCheck />} label="Compras aprovadas" />
+        <ResumeCard icon={<BsCartX />} label="Compras recusadas" />
+        <ResumeCard icon={<AiOutlineSwap />} label="% Conversões" />
+      </Box>
+    </MainContent>
+  );
 };
