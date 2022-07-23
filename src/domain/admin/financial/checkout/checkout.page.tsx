7ddbdffb161434f5493fecddb675dfd915cfa802
Matheus Ferreira <teteu.andrade06@gmail.com>
2022-07-23 11:52:05 -0300

    feat: moving recipe components to checkout

--- src/domain/admin/financial/checkout/checkout.page.tsx
+++ src/domain/admin/financial/checkout/checkout.page.tsx
@@ -3,9 +3,11 @@ import { ResumeCard } from '@domain/admin/components/resume-card/resume-card.com
 import { Box } from '@mui/material';
 import { useMedia } from '@shared/hooks/useMedia';
 import {
+  AiOutlineCheck,
   AiOutlineFileDone,
   AiOutlineFileExcel,
   AiOutlineSwap,
+  AiOutlineUpload,
 } from 'react-icons/ai';
 import { BsCartCheck, BsCartX } from 'react-icons/bs';
 import { TbFileInvoice } from 'react-icons/tb';
@@ -31,6 +33,36 @@ export const Checkout = () => {
           icon={<AiOutlineFileExcel />}
           label="Checkouts cancelados"
         />
+      </Box>
+
+      <CardTitle>Boletos</CardTitle>
+      <Box
+        style={{
+          width: '100%',
+          maxWidth: '1200px',
+          display: 'flex',
+          gap: '2rem',
+          flexWrap: mobile ? 'wrap' : 'initial',
+        }}
+      >
+        <ResumeCard icon={<AiOutlineUpload />} label="Gerados" />
+        <ResumeCard icon={<AiOutlineCheck />} label="Pagos" />
+        <ResumeCard icon={<AiOutlineSwap />} label="% Conversões" />
+      </Box>
+
+      <CardTitle>Pix</CardTitle>
+      <Box
+        style={{
+          width: '100%',
+          maxWidth: '1200px',
+          display: 'flex',
+          gap: '2rem',
+          flexWrap: mobile ? 'wrap' : 'initial',
+        }}
+      >
+        <ResumeCard icon={<AiOutlineUpload />} label="Gerados" />
+        <ResumeCard icon={<AiOutlineCheck />} label="Pagos" />
+        <ResumeCard icon={<AiOutlineSwap />} label="% Conversões" />
       </Box>
 
       <CardTitle>Cartão de crédito</CardTitle>
